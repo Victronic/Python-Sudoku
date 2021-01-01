5097cfbde6753822105578f57eeb138857e38c17
Victor <44725192+Victronic@users.noreply.github.com>
2021-01-01 16:06:18 +0200

    drawBoard
    Some preparation of the board and
    game window

---
+++ SudokuGame/attributes.py
@@ -0,0 +1,18 @@
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# boards
+testBoard = [[0 for x in range(9)] for y in range(9)]
+
+# positions
+gridPos = (75, 100)
+
+# allSizes
+
+# windowSizes
+WIDTH = 600
+HEIGHT = 600
+
+# cellSize
+cellSize = 50
